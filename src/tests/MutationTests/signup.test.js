9271b5d6464029ddba8732954dceba3d0e4005d6
Leonard Eyer <le0nard@me.com>
2018-05-13 19:28:07 +0200

    first signup test

---
+++ src/tests/MutationTests/signup.test.js
@@ -0,0 +1,19 @@
+const { signup } = require('../../resolvers/Mutation')
+const bcrypt = require('bcryptjs')
+
+const store = []
+const db = {
+    mutation: {
+        createUser: ({ data }, _) => ({
+            token: store.push(data) - 1,
+            user: data
+        })        
+    }
+}
+const mockSignup = args => signup(null, args, { db }, null)
+
+test('Inserts new user correctly', async () => {
+    const { user } = await mockSignup({ email: 'ab@c.de', password: 'password123' })
+    expect(store[0].email).toEqual('ab@c.de')
+    expect(await bcrypt.compare(store[0].password, 'password123')).toBeTruthy
+})
