253cb6cad9fcccbabff63044e42a6266e8aff620
Leonard Eyer <le0nard@me.com>
2018-05-13 19:47:18 +0200

    First login test

--- src/tests/MutationTests/login.test.js
+++ src/tests/MutationTests/login.test.js
@@ -1,7 +1,51 @@
-const { expect } = require('chai')
+const { login } = require('../../resolvers/Mutation')
+const { getUserId } = require('../../utils')
+const bcrypt = require('bcryptjs')
+const lodash = require('lodash')
 
-describe('Testing Login', () => {
-    it('Does not login non existing user', () => {
-        expect(1).to.equal(1)
+// Our DB mock
+let store = []
+const db = {
+    mutation: {
+        createUser: ({ data }, _) => ({
+            token: store.push(data) - 1,
+            user: data
+        })
+    },
+    query: {
+        user: ({ where }) => lodash.filter(store, where)[0]
+    }
+}
+const mockLogin = args => login(null, args, { db }, null)
+
+// Reset DB before every test
+beforeAll(() => store = [])
+
+test('Does not login non existing user', () => {
+    return expect(mockLogin({ email: 'ab@c.de', password: 'password123' }))
+        .rejects.toThrow('No such user found')
+})
+
+test('Invalid password fails', async() => {
+    // Create a User
+    store.push({
+        email: 'ab@c.de',
+        password: await bcrypt.hash('password123', 10)
     })
+    // Try login
+    await expect(mockLogin({ email: 'ab@c.de', password: 'invalid' }))
+        .rejects.toThrow('Invalid password')
+})
+
+test('Does login existing user', async () => {
+    // Create a User
+    store.push({
+        email: 'ab@c.de',
+        password: await bcrypt.hash('password123', 10)
+    })
+    // Try login
+    const { token, user } = await mockLogin({ email: 'ab@c.de', password: 'password123' })
+    expect(store[0].email).toEqual('ab@c.de')
+    expect(await bcrypt.compare(store[0].password, 'password123')).toBeTruthy
+    expect(getUserId({ request: { get: () => token } })).toEqual(user.id)
 })
