4669cdef8a21fb4c05b7a1a56bbd474750a52059
Leonard Eyer <le0nard@me.com>
2018-05-13 19:30:54 +0200

    signup test now tests for valid jwt

--- src/tests/MutationTests/signup.test.js
+++ src/tests/MutationTests/signup.test.js
@@ -1,4 +1,5 @@
 const { signup } = require('../../resolvers/Mutation')
+const { getUserId } = require('../../utils')
 const bcrypt = require('bcryptjs')
 
 const store = []
@@ -7,13 +8,14 @@ const db = {
         createUser: ({ data }, _) => ({
             token: store.push(data) - 1,
             user: data
-        })        
+        })
     }
 }
 const mockSignup = args => signup(null, args, { db }, null)
 
 test('Inserts new user correctly', async () => {
-    const { user } = await mockSignup({ email: 'ab@c.de', password: 'password123' })
+    const { token, user } = await mockSignup({ email: 'ab@c.de', password: 'password123' })
     expect(store[0].email).toEqual('ab@c.de')
     expect(await bcrypt.compare(store[0].password, 'password123')).toBeTruthy
+    expect(getUserId({ request: { get: () => token } })).toEqual(user.id)
 })
