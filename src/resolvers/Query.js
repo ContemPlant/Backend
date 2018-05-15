2a55596ee637b823d2469364e08558ab9e5a72cf
Leonard Eyer <le0nard@me.com>
2018-05-15 22:22:07 +0200

    plants query now only returning user plants

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,15 @@
+const { getUserId } = require('../utils')
 
-const info = () => `This is the API of a Hackernews Clone`
+const info = () => `This is the API of our PlantsDb`
 
-const plants = (root, args, context, info) => 
-    context.db.query.plants({}, info)
+const plants = (root, args, context, info) =>
+    context.db.query.plants({
+        where: {
+            owner: { id: getUserId(context) }
+        }
+    }, info)
+
+
 
 module.exports = {
     info, plants
