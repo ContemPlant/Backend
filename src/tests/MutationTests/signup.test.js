1396f0fcaa6185d5027e1c8da6efa297cd4d884a
Leonard Eyer <le0nard@me.com>
2018-05-22 19:59:21 +0200

    updated test

--- src/tests/MutationTests/signup.test.js
+++ src/tests/MutationTests/signup.test.js
@@ -14,7 +14,7 @@ const db = {
 const mockSignup = args => signup(null, args, { db }, null)
 
 test('Inserts new user correctly', async () => {
-    const { token, user } = await mockSignup({ email: 'ab@c.de', password: 'password123' })
+    const { token, user } = await mockSignup({ email: 'ab@c.de', password: 'password123', username: 'Hansi' })
     expect(store[0].email).toEqual('ab@c.de')
     expect(await bcrypt.compare(store[0].password, 'password123')).toBeTruthy
     expect(getUserId({ request: { get: () => token } })).toEqual(user.id)
