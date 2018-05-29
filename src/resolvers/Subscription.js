3baa8840e1b56989c87d0951d968815a927d7e23
Leonard Eyer <le0nard@me.com>
2018-05-29 21:21:14 +0200

    Implement Subscription on sensorData

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -2,32 +2,42 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId, matchType } = require('../utils')
 
-async function *sensorsData(parent, args, context, info) {
-
-    const sensorData = await context.db.subscription['sensor' + matchType(args.type)](
-        { where: args.where, plant: { id: args.plantId } },
-        info,
-    )
-
-    console.log(sensorData)
+/**
+ * Subscription on a table of sensorData<type>
+ */
+function newSensorData(parent, args, context, info) {
+    return context.db.subscription['sensor' + matchType(args.type)]({
+        where: {
+            mutation_in: ['CREATED'],
+            node: { plant: { id: args.plantId } }
+        }
+    }, `{ node { value, timeStamp } }`)
+}
+/**
+ * Subscription on the ardu table
+ */
+function arduChange(parent, args, context, info) {
+    return context.db.subscription.ardu({
+        where: args.where
+    }, info)
 }
 
-function arduSubscribe(parent, args, context, info) {
-    return context.db.subscription.ardu(
-        { where: args.where },
-        info,
-    )
-}
-
-const newSensorData = {
-    subscribe: sensorsData
-}
-
-const arduChange = {
-    subscribe: arduSubscribe
+/**
+ * Resolves the sensordata changes to a SensorSubscriptionPayload
+ */
+function resolveNewSensorData(parent, args, context, info) {
+    const description = `sensor${matchType(args.type)}`
+    const values = parent[description].node
+    return {
+        type: args.type,
+        ...values
+    }
 }
 
 module.exports = {
-    newSensorData,
-    arduChange
+    arduChange: { subscribe: arduChange },
+    newSensorData: {
+        subscribe: newSensorData,
+        resolve: resolveNewSensorData
+    }
 }
