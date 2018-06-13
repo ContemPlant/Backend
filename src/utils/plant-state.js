c8589b0ef240aee9a5ab0d9b6f80ac485d5e7feb
Leonard Eyer <le0nard@me.com>
2018-06-13 17:47:03 +0200

    Add comments

--- src/utils/plant-state.js
+++ src/utils/plant-state.js
@@ -4,27 +4,53 @@ const lim = require('./limit.js')({ min: 0 })({ max: 1 })
  * @module Utils
  */
 
-function environment(weights, values, optimas, maxDeviations = [10, 20, 100, 100]) {
+/**
+ * Calculates an environment evaluation
+ * @param {Array<Float>} values Sensor values
+ * @param {Array<Float>} weights Weights for optimas
+ * @param {Array<Float>} optimas Optimal sensor value
+ * @param {Array<Float>} maxDeviations Max deviation values
+ */
+function environment(values, weights, optimas, maxDeviations = [10, 20, 100, 100]) {
 
+    const weightSum = lo.sum(weights)
+    weights = weights.map(weight => weight / weightSum)
+
+    // Gaussian bell like function
     const fnct = (max) => (deviation) =>
         deviation > max
             ? 0
             : 2 / Math.pow(max, 3) * Math.pow(deviation, 3)
             - (3 / Math.pow(max, 2) * Math.pow(deviation, 2)) + 1
 
+    // Create functions for every max deviation
     const fncts = maxDeviations.map(fnct)
 
     const combinator = (weight, value, optimum, fn) =>
         weight * fn(Math.abs(optimum - value))
 
+    // Combine all parameters with combinator and sum everything
     return lo.sum(lo.zipWith(weights, values, optimas, fncts, combinator))
 }
 
+/**
+ * Calculates the new health value for given environment
+ * @param {Float} oldHealth 
+ * @param {Float} currentEnvironment 
+ * @param {Float} factor 
+ */
 function health(oldHealth, currentEnvironment, factor = 0.5) {
     return lim(oldHealth * factor + currentEnvironment * (1 - factor))
 }
 
-function growth(oldSize, currentHealth, threshold = 0.5, factor = 0.5) {
+/**
+ * Calculates the new size for given health
+ * @param {Float} oldSize
+ * @param {Float} currentHealth 
+ * @param {Float} threshold Threshold when to start increasing in size
+ * @param {Float} factor 
+ */
+function plantSize(oldSize, currentHealth, threshold = 0.5, factor = 0.5) {
     return currentHealth > threshold
         ? oldSize + currentHealth * factor
         : oldSize
@@ -34,5 +60,5 @@ function growth(oldSize, currentHealth, threshold = 0.5, factor = 0.5) {
 module.exports = {
     environment,
     health,
-    growth
+    plantSize
 }
