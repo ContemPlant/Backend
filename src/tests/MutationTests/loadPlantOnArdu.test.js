3e0b373a2ff09997f52de096426a986f087d9ecc
Leonard Eyer <le0nard@me.com>
2018-06-14 22:10:43 +0200

    Update loadplant test

--- src/tests/MutationTests/loadPlantOnArdu.test.js
+++ src/tests/MutationTests/loadPlantOnArdu.test.js
@@ -1,71 +1,61 @@
-const lodash = require('lodash')
-const jwt = require('jsonwebtoken')
 const utils = require('../../utils')
+const Mutations = require('../../resolvers/Mutation')
 
-const { loadPlantOnArdu } = require('../../resolvers/Mutation')({ utils })
-const { APP_SECRET } = utils
-
-// *********** Begin mock stuff **************
-const store = {
-    plantStore: [],
-    arduStore: []
-}
-const resetStore = store => {
-    store.plantStore = []
-    store.arduStore = []
-}
 const db = {
     mutation: {
-        updateArdu: async ({ where, data }, _) => {
-            store.arduStore = store.arduStore.map(x =>
-                (x.id == where.id)
-                    ? { ...x, ...data }
-                    : x
-            )
-            return lodash.filter(store.arduStore, where)[0]
-        }
-    },
-    query: {
-        plant: async ({ where }, _) => lodash.filter(store.plantStore, where)[0]
+        updateArdu: jest.fn()
     }
 }
-const mockLoadPlant = context => args => loadPlantOnArdu(null, args, context, null)
-// ************ End mock stuff ****************
 
-// Before each test reset the store
-beforeEach(() => resetStore(store))
+test('Loads plant (client has permission, non loaded plant)', async () => {
 
-test('Loads plant', async () => {
+    const mockUtils = {
+        ...utils,
+        getUserId: () => 'user1',
+        hasPlantEditPermissionInContext: () => true,
+        arduPlantIsLoadedToInContext: () => null
+    }
 
-    // Setup store
-    store.plantStore.push({ id: 'plant1', owner: { id: 'user1' } })
-    store.arduStore.push({ arduId: 'ardu1', loadedPlant: null })
+    const { loadPlantOnArdu: mockLoadPlant } = Mutations({ utils: mockUtils })
 
-    const token = jwt.sign({ userId: 'user1' }, APP_SECRET)
+    const args = { plantId: 'plant1', arduId: 'ardu1' }
+    const ardu = await mockLoadPlant(null, args, { db })
 
-    const ardu = await mockLoadPlant({
-        db, request: { get: () => token }
-    })({
-        plantId: 'plant1',
-        arduId: 'ardu1'
+    expect(db.mutation.updateArdu.mock.calls[0][0]).toEqual({
+        data: { loadedPlant: { connect: { id: "plant1" } } },
+        where: { arduId: "ardu1" }
     })
-
-    expect(ardu.loadedPlant.connect.id).toEqual('plant1')
 })
 
-test('Does not load plant that user doesn`t own', () => {
+test('Does not load plant (client has no permission)', () => {
 
-    // Setup store
-    store.plantStore.push({ id: 'plant1', owner: { id: 'user1' } })
-    store.arduStore.push({ id: 'ardu1', loadedPlant: null })
+    const mockUtils = {
+        ...utils,
+        getUserId: () => 'user1',
+        hasPlantEditPermissionInContext: () => false,
+        arduPlantIsLoadedToInContext: () => null
+    }
 
-    const token = jwt.sign({ userId: 'user2' }, APP_SECRET)
-    const arduPromise = mockLoadPlant({
-        db, request: { get: () => token }
-    })({
-        plantId: 'plant1',
-        arduId: 'ardu1'
-    })
-    expect(arduPromise).rejects.toThrowError("Client does not have permisson to load plant")
+    const { loadPlantOnArdu: mockLoadPlant } = Mutations({ utils: mockUtils })
+    const args = { plantId: 'plant1', arduId: 'ardu1' }
+    const arduPromise = mockLoadPlant(null, args, { db })
+
+    expect(arduPromise).rejects.toThrowError('Client does not have permisson to load plant')
+})
+
+test('Does not load plant (Plant already loaded)', () => {
+
+    const mockUtils = {
+        ...utils,
+        getUserId: () => 'user1',
+        hasPlantEditPermissionInContext: () => true,
+        arduPlantIsLoadedToInContext: () => 'notNull'
+    }
+
+    const { loadPlantOnArdu: mockLoadPlant } = Mutations({ utils: mockUtils })
+    const args = { plantId: 'plant1', arduId: 'ardu1' }
+    const arduPromise = mockLoadPlant(null, args, { db })
+
+    expect(arduPromise).rejects.toThrowError('Plant is already loaded to another ardu!')
 })
 
